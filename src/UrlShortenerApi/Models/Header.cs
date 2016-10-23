bf9302f09b627ad087fc9c2d61451f000a4246dc
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-10-23 14:43:59 -0400

    Add Url and Header models (first draft)

---
+++ src/UrlShortenerApi/Models/Header.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UrlShortenerApi.Repositories
+{
+    public class Header
+    {
+        public int ID { get; set; }
+        public string UserAgent { get; set; }
+        public string RequestIp { get; set; }
+        public int RequestCount { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int UrlID { get; set; }
+        public Url Url { get; set; }
+    }
+}
---
+++ src/UrlShortenerApi/Models/Url.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UrlShortenerApi.Repositories
+{
+    public class Url
+    {
+        public int ID { get; set; }
+        public string LongFormat { get; set; }
+        public string ShortFormat { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatedByIp { get; set; }
+        public ICollection<Header> Headers { get; set; }
+    }
+}
